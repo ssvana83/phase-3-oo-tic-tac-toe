f802025514e0c9209b2ad449c7c51ff1dc3c3c87
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-03-07 14:20:20 -0500

    commit tree b37656cbcff08199540bf65e9c54aa553d772203

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,49 @@
+class TicTacToe
+
+  def initialize
+    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[6,4,2],[0,4,8]]
+
+  def display_board
+    p @board[0,3]
+    p "-----------"
+    p @board[3,3]
+    p "-----------"
+    p @board[6,3]
+  end
+
+  def input_to_index(string)
+    @choice = string.to_i-1
+  end
+
+  def move(input_to_index,player)
+    @board [input_to_index] = player 
+  end
+
+  def position_taken?(input_to_index)
+    if @board[input_to_index] == 'X' || @board[input_to_index] == 'O'
+      true
+    else
+      false
+    end
+  end
+
+  def valid_move?(input_to_index)
+    !position_taken?(input_to_index) && @board[input_to_index]
+  end
+
+  def turn_count
+    count = 0
+    @board.each.do |i|
+      count +=1 if i == "X" || i == "O"
+    end
+  count
+  end
+
+  def current_player
+    turn_count %2 == 0 ? "X" : "O"
+  end
+
+end
